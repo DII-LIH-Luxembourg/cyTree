e3adc9159a07034f21734445bdfd21c242094cd0
ohunewald <oliver.hunewald@lih.lu>
2025-07-21 14:15:42 +0200

    corrected man node.rd and update node.r

--- R/node.R
+++ R/node.R
@@ -1,18 +1,22 @@
-#' Node class for cyTree
+#' @title Node Class for cyTree
+#' @name Node
 #'
-#' Represents a tree node with name, ID, parent, children, and marker annotations.
+#' @description
+#' A tree node used to annotate CyTOF cluster hierarchies. Each node contains a unique ID,
+#' a name, marker metadata, and pointers to its parent and children.
 #'
 #' @export
 Node <- R6::R6Class("Node",
                     public = list(
-                      id = NULL,
-                      name = NULL,
-                      parent = NULL,
-                      children = list(),
-                      positive_markers = list(),
-                      negative_markers = list(),
 
-                      initialize = function(id, name, parent = NULL, positive_markers = list(), negative_markers = list()) {
+                      #' @description Create a new Node
+                      #' @param id Character. Unique ID for the node.
+                      #' @param name Character. Name/label of the node.
+                      #' @param parent Node or NULL. Parent node.
+                      #' @param positive_markers List of marker names that are positive.
+                      #' @param negative_markers List of marker names that are negative.
+                      initialize = function(id, name, parent = NULL,
+                                            positive_markers = list(), negative_markers = list()) {
                         self$id <- id
                         self$name <- name
                         self$parent <- parent
@@ -21,37 +25,44 @@ Node <- R6::R6Class("Node",
                         self$children <- list()
                       },
 
+                      #' @description Add a child node
+                      #' @param child_node A Node object to add as a child.
                       add_child = function(child_node) {
                         stopifnot(inherits(child_node, "Node"))
-                        child_node$parent <- self
                         self$children <- c(self$children, list(child_node))
+                        child_node$parent <- self
                       },
 
+                      #' @description Get full path from root to this node
                       get_path = function() {
-                        if (is.null(self$parent)) {
-                          return(self$name)
-                        } else {
-                          return(paste0(self$parent$get_path(), " > ", self$name))
-                        }
+                        if (is.null(self$parent)) return(self$name)
+                        paste0(self$parent$get_path(), " > ", self$name)
                       },
 
+                      #' @description Recursively convert node and children to nested list
                       to_list = function() {
                         list(
                           id = self$id,
                           name = self$name,
-                          positive_markers = self$positive_markers,
-                          negative_markers = self$negative_markers,
+                          metadata = list(
+                            positive_markers = self$positive_markers,
+                            negative_markers = self$negative_markers
+                          ),
                           children = lapply(self$children, function(c) c$to_list())
                         )
                       },
 
+                      #' @description Print the node as a tree
+                      #' @param indent Indentation level (for pretty printing)
                       print_node = function(indent = 0) {
-                        cat(paste0(strrep("  ", indent), "- ", self$name, " (ID: ", self$id, ")\n"))
+                        cat(strrep("  ", indent), "- ", self$name, "\n", sep = "")
                         for (child in self$children) {
                           child$print_node(indent + 1)
                         }
                       },
 
+                      #' @description Find a node by ID
+                      #' @param id Character. ID to search for.
                       find_by_id = function(id) {
                         if (self$id == id) return(self)
                         for (child in self$children) {
@@ -61,6 +72,8 @@ Node <- R6::R6Class("Node",
                         return(NULL)
                       },
 
+                      #' @description Find a node by name
+                      #' @param name Character. Name to search for.
                       find_by_name = function(name) {
                         if (self$name == name) return(self)
                         for (child in self$children) {
@@ -70,11 +83,11 @@ Node <- R6::R6Class("Node",
                         return(NULL)
                       },
 
+                      #' @description Return data.frames of all nodes and edges
                       to_dataframes = function() {
                         nodes <- data.frame()
                         edges <- data.frame()
 
-                        # Recursive helper function
                         walk_tree <- function(node) {
                           node_row <- data.frame(
                             id = node$id,
@@ -94,6 +107,5 @@ Node <- R6::R6Class("Node",
                         walk_tree(self)
                         return(list(nodes = nodes, edges = edges))
                       }
-
                     )
 )
